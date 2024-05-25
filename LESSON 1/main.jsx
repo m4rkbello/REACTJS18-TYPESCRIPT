9e994473dc6663b59c7029ad518949c47329160d
m4rkbello <markbello10041999@gmail.com>
2024-05-25 18:46:28 +0800

    PRIMITIVE VALUE AND REFERENCE VALUE

--- LESSON 1/main.jsx
+++ LESSON 1/main.jsx
@@ -27,10 +27,40 @@ if(true){
 // console.log(typescript);
 
 
-
 //FUNCTION REFACTORY SYNTAX
 // for(let ez = 0; ez < 3; ez++){
 //     (function test(o){
 //         alert(o);
 //     })(ez)
 // }
+
+
+//PRIMITIVE TYPES VALUES   
+let firstNumber = 100;
+let copyFirstNumber = firstNumber;
+firstNumber = 1000;
+
+console.log("TEST 1",firstNumber);
+console.log("TEST 2", copyFirstNumber);
+
+
+//REFERENCE TYPES VALUES
+let firstArray = [1];
+let copyFirstArray = firstArray;
+copyFirstArray.push(2);
+
+console.log("ARRAY 1", firstArray);
+console.log("ARRAY 2", copyFirstArray);
+
+
+//PREMITIVE TYPES WITH OBJECTS
+
+let personalDetails  = { 
+    firstname: 'mark',
+    lastname: 'bello',
+    age: '25'
+};
+let bioData = personalDetails;
+personalDetails.work = 'tig lung-ag';
+
+console.log(bioData);
