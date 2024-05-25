35e1ff5ac8d10751c5eae7bc38a97abd2cf87cd8
m4rkbello <markbello10041999@gmail.com>
2024-05-25 20:00:28 +0800

    OBJECTS AND ARRAYS COPYING USING SHALLOW COPY AND DEEP COPY!

--- LESSON 1/main.jsx
+++ LESSON 1/main.jsx
@@ -11,7 +11,6 @@
 // typescript();
 
 
-
 //FUNCTION BLOCK SCOPE 
 if(true){
     var typescript = 'typescript';
@@ -36,31 +35,71 @@ if(true){
 
 
 //PRIMITIVE TYPES VALUES   
-let firstNumber = 100;
-let copyFirstNumber = firstNumber;
-firstNumber = 1000;
-
-console.log("TEST 1",firstNumber);
-console.log("TEST 2", copyFirstNumber);
+// let firstNumber = 100;
+// let copyFirstNumber = firstNumber;
+// firstNumber = 1000;
+// console.log("TEST 1",firstNumber);
+// console.log("TEST 2", copyFirstNumber);
 
 
 //REFERENCE TYPES VALUES
-let firstArray = [1];
-let copyFirstArray = firstArray;
-copyFirstArray.push(2);
-
-console.log("ARRAY 1", firstArray);
-console.log("ARRAY 2", copyFirstArray);
+// let firstArray = [1];
+// let copyFirstArray = firstArray;
+// copyFirstArray.push(2);
+// console.log("ARRAY 1", firstArray);
+// console.log("ARRAY 2", copyFirstArray);
 
 
 //PREMITIVE TYPES WITH OBJECTS
+// let personalDetails  = { 
+//     firstname: 'mark',
+//     lastname: 'bello',
+//     age: '25'
+// };
+// let bioData = personalDetails;
+// personalDetails.work = 'tig lung-ag';
+// console.log(bioData);
 
-let personalDetails  = { 
-    firstname: 'mark',
-    lastname: 'bello',
-    age: '25'
-};
-let bioData = personalDetails;
-personalDetails.work = 'tig lung-ag';
 
-console.log(bioData);
+//OBJECTS AND ARRAYS COPYING USING SHALLOW COPY AND DEEP COPY!
+
+//reference type deep copy
+// const data = {
+//     test: 'test',
+//     userDetails: {
+//         address: 
+//         {
+//             municipality: 'montevista',
+//             province: 'davao de oro',
+//             country: 'philippines'
+//         }
+//     }
+// }
+
+// const copy = data;
+// data.test = 'PADAYON!';
+
+// console.log("ORIGINAL", data);
+// console.log("COPY", copy);
+
+
+const data = {
+    test: 'test',
+    userDetails: {
+        address: 
+        {
+            municipality: 'montevista',
+            province: 'davao de oro',
+            country: 'philippines'
+        }
+    }
+}
+
+//gamit ug Object.assign(()); method
+const copy = Object.assign({}, data);
+
+data.test = 'PADAYON!';
+
+console.log("COPY", copy);
+console.log("ORIGINAL", data);
+
