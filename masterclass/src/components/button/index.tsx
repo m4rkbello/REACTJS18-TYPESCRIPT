876a90402ffdca723c2dc486f0e31e451d84a334
m4rkbello <markbello10041999@gmail.com>
2024-06-03 22:31:55 +0800

    ADD STYLES AND CLASS IN COMPONENT

--- masterclass/src/components/button/index.tsx
+++ masterclass/src/components/button/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 
-
 interface IButtonProps {
     children: React.ReactNode;
     onClick: () => void;
+    //optional = ?
+    type?: "primary" | "default";
 }
 
-
-export const Button: React.FC <IButtonProps> = ({children, onClick}) => {
-
+export const Button: React.FC <IButtonProps> =
+ ({children, onClick, type}) => {
     //FUCNTION
     const handleClickBtn = () => {
         console.log("CLICKED!");
     };
 
+    const className = type === "primary" ? "btn-primary" : "";
+
     return(
     <React.Fragment>
-        <button onClick={onClick}>{children}</button>
+        <button 
+            // style={{ 
+            //    height: '20px',
+            //  }} 
+            className={className}
+            onClick={onClick}>{children}
+        </button>
     </React.Fragment>
     );
 };
