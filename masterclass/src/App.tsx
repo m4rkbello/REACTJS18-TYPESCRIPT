0b27281ab7218b7181167e5bbefee9a83a90f5a1
m4rkbello <markbello10041999@gmail.com>
2024-06-03 21:46:04 +0800

    COMPONENTS & PROPS

--- masterclass/src/App.tsx
+++ masterclass/src/App.tsx
@@ -1,11 +1,15 @@
-
+import React, {isValidElement} from 'react';
 import {Button} from '../src/components/button/index'; // Assuming Button is a default export
 
 function App() {
+
+  console.log("isValidElement", isValidElement(<p/>))
+
+
   return (
     <div className="App">
       <h1>REACT+TYPESCRIPT</h1>
-      <Button />
+      <Button onClick={() => console.log("reusable handleClick")}>Click</Button>
     </div>
   );
 }
