0a0b7b4f4c568ee1a74fd98ed0b778bacf593a68
m4rkbello <markbello10041999@gmail.com>
2024-06-04 00:56:03 +0800

    tug ta tyg

---
+++ masterclass/src/components/Post/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+interface IPostProps {
+    showDate: boolean;
+};
+
+export const Post: React.FC <IPostProps> = ({showDate}) => {
+    const date = new Date().toDateString();
+
+    if(showDate){
+        return (
+            <div>
+                <span>MY POST</span>
+                <p>{date}</p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <span>MY POST</span>
+        </div>
+    )
+
+};
