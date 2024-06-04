967d022627c7254d0d72f3ee19294b58c576cd02
m4rkbello <markbello10041999@gmail.com>
2024-06-04 22:07:53 +0800

    LIFE CYCLE: WITH MOUNT, UPDATE, UNMOUNT

---
+++ masterclass/src/components/ChildComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ChildComponent = () => {
+    console.log("CHILD COMPONENT RENDER BCOZ IS TRUE!!");
+
+    return(
+        <div>
+            CHILD COMPONENT CHOI!
+        </div>
+    )
+}
