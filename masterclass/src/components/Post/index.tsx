7aee6695181efd788c32e6f0c251c1a5550cf010
m4rkbello <markbello10041999@gmail.com>
2024-06-04 21:24:00 +0800

    CONDITIONAL RENDERING

--- masterclass/src/components/Post/index.tsx
+++ masterclass/src/components/Post/index.tsx
@@ -5,21 +5,38 @@ interface IPostProps {
     showDate: boolean;
 };
 
-export const Post: React.FC <IPostProps> = ({showDate}) => {
+export const Post: React.FC <IPostProps> = ({ showDate }) => {
     const date = new Date().toDateString();
-    if(showDate){
-        return (
-            <div>
-                <span>MY POST</span>
-                <p>{date}</p>
-            </div>
-        )
-    }
 
+    //gamit conditional statement sa compo
+    // if(showDate){
+    //     return (
+    //         <div>
+    //             <span>MY POST</span>
+    //             <p>{date}</p>
+    //         </div>
+    //     )
+    // }
+
+    // conditional rendering gamit AND
     return (
         <div>
-            <span>MY POST</span>
+               <span>MY POST</span>
+               {showDate && <p>{date}</p> }
         </div>
     )
+
+    const renderComp = [<span style={{ fontSize: '50px',  textAlign: 'center'}}> PATOTOYA</span>];
+
+    // if(showDate){
+    //     renderComp.push(<p>{date}</p>)
+    // }
+
+    // return (
+
+    //     <React.Fragment>
+    //         {renderComp}
+    //     </React.Fragment>
+    // )
 
 };
