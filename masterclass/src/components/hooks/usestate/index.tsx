d4b6651a5235db6141d59ad59a08dac19a375112
m4rkbello <markbello10041999@gmail.com>
2024-06-06 23:05:13 +0800

    NONE

--- masterclass/src/components/hooks/usestate/index.tsx
+++ masterclass/src/components/hooks/usestate/index.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
+const calculateCounter = () => {
+    console.log(calculateCounter);
+    return 1000;
+}
 
-export const index = () => {
-    
+export const UseStateHook = () => {
+    //useState
+    const [counter, setCounter] = useState(calculateCounter);
+    const handleAdd = () =>{
+        // setCounter(counter + 1);
+        // setCounter((counter) => counter+1);
+        // setCounter((counter) => counter+1);
+    };
+
+return (
+    <div>
+        <h1>Counter: {counter}</h1>
+        <button onClick={handleAdd}>Increment</button>
+    </div>
+)
+
 }
