26e2c7ba80386355768389cfc8ccb4a7640ed968
m4rkbello <markbello10041999@gmail.com>
2024-06-12 21:20:37 +0800

    USE REDUCER

---
+++ masterclass/src/components/hooks/useReducer/ChildIndex.tsx
@@ -0,0 +1,14 @@
+import React, { useReducer } from "react";
+import { Reducer } from "redux";
+
+
+export const  ChildIndex = () => {
+
+
+    console.log("CHILD INDEX");
+    return(
+        <div>
+            <h1>CHILD REDUCER INDEX</h1>
+        </div>
+    )
+}
