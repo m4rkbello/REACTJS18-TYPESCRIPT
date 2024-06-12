1d253960bab8cec39d6bde8e8a3f2786026719b5
m4rkbello <markbello10041999@gmail.com>
2024-06-13 00:07:05 +0800

    useReducer + REDUX PATTERN

--- masterclass/src/components/hooks/useReducer/Datas.tsx
+++ masterclass/src/components/hooks/useReducer/Datas.tsx
@@ -1,40 +1,90 @@
-import React from "react";
-
+import React, { useReducer, useState, Reducer } from "react";
 
 interface Idata {
-    name: string,
-    quantity: number,
+    name: string;
+    quantity: number;
 }
 
+interface IdataAction {
+    type: "ADD_DATA" | "REMOVE_DATA";
+    name: string;
+    quantity: number;
+}
+
+const reducer: Reducer<Idata[], IdataAction> = (state, action) => {
+    switch (action.type) {
+        case "ADD_DATA":
+            return [
+                ...state,
+                { name: action.name, quantity: action.quantity }
+            ];
+        case "REMOVE_DATA":
+            return state.filter(item => item.name !== action.name);
+        default:
+            return state;
+    }
+};
 
 export const DataArrays = () => {
+    // useState 
+    const [inputName, setInputName] = useState("");
+    const [inputQuantity, setInputQuantity] = useState(0);
 
-    const DataArrays: Idata[] = [{
+    // useReducer
+    const [dataArrays, dispatch] = useReducer(reducer, [{
         name: "MARK BELLO",
         quantity: 10041999,
-    }];
+    }]);
 
-    console.log("DATAARRAYS RENDER!")
+    // Handle input change for name
+    const handleInputNameOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        setInputName(e.currentTarget.value);
+    };
+
+    // Handle input change for quantity
+    const handleQuantityOnChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        setInputQuantity(+e.currentTarget.value);
+    };
+
+    // Function to add data
+    const handleAddButtonClick = () => {
+        dispatch({
+            type: "ADD_DATA",
+            name: inputName,
+            quantity: inputQuantity,
+        });
+    };
+
+    // Function to remove data
+    const handleRemoveButtonClick = (name: string) => {
+        dispatch({
+            type: "REMOVE_DATA",
+            name: name,
+            quantity: 0,
+        });
+    };
+
+    console.log("DATAARRAYS RENDER!");
 
     return (
         <div>
             <h1>
-            USE REDUCER ADVANCE + REDUX PATTTERN!
+                USE REDUCER ADVANCE + REDUX PATTERN!
             </h1>
             <ul>
-                {DataArrays.map(data =>{
-                    return(
-                        <li key={data.name}>
-                            {data.name} + {data.quantity}
-                            {" "}<button>REMOVE</button>
+                {dataArrays.map(item => {
+                    return (
+                        <li key={item.name}>
+                            {item.name} + {item.quantity}
+                            {" "}
+                            <button onClick={() => handleRemoveButtonClick(item.name)}>REMOVE</button>
                         </li>
-                    )
+                    );
                 })}
             </ul>
-            Name: <input /><br />
-            Quantity: <input /><br />
-            <button>Add Data</button>
-            
+            Name: <input onChange={handleInputNameOnChange} value={inputName} type="text" /><br />
+            Quantity: <input onChange={handleQuantityOnChange} value={inputQuantity} type="number" /><br />
+            <button onClick={handleAddButtonClick}>Add Data</button>
         </div>
-    )
+    );
 };
