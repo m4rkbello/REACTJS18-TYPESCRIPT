86b896acac45c18932bdce25c5b4bc70b2a60c12
m4rkbello <markbello10041999@gmail.com>
2024-06-12 20:20:32 +0800

    CONTROLLED AND UNCONTROLLED COMPONENT

--- masterclass/src/components/controlleduncontrolledcompponents/index.tsx
+++ masterclass/src/components/controlleduncontrolledcompponents/index.tsx
@@ -4,20 +4,19 @@ export const ControlledUncontrolledComponents = () => {
 
     const datas = ["MARK", "EVAN", "MARKO", "CHARLES","JOSHUA"];
 
-    const [input, setInput] = useState<number>(1);
+    const [input, setInput] = useState("");
 
     const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        setInput(e.target.value); // Use target instead of currentTarget
+            setInput(e.currentTarget.value);
     };
     
-
     return (
         <div>
             <h1>CONTROLLED AND UNCONTROLLED COMPONENT!</h1>
             Search: <input onChange={handleInputChange} value={input} />
             <ul>
-                {datas.map(item => 
-                    (<li key={item}>{item}</li>)
+                {datas.filter(data => data.includes(input)).map(data => 
+                    (<li key={data}>{data}</li>)
                 )}
             </ul>
             <button onClick={() => setInput(input+1)} >ADD</button>
