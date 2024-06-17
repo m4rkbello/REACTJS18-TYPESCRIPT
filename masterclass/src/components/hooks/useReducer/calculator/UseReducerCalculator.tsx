247eb56b425f099075fa22099f01972bbe62c76b
m4rkbello <markbello10041999@gmail.com>
2024-06-17 17:56:57 +0800

    useReducer

--- masterclass/src/components/hooks/useReducer/calculator/UseReducerCalculator.tsx
+++ masterclass/src/components/hooks/useReducer/calculator/UseReducerCalculator.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, {Reducer, useReducer} from "react";
 import { AddUserReducer } from "./add/AddUseReducer";
 import { SubtractUseReducer } from "./subtract/SubtractUseReducer";
 import { DevideUseReducer } from "./devide/DevideUseReducer";
 import { MultiplyUseReducer } from "./multiply/MultiplyUseReducer";
 
 
+export interface IAction {
+    type: "ADD" | "SUBTRACT" | "MULTIPLY" | "DEVIDE";
+    number: number;
+}
 
+const reducer: Reducer<number, IAction> = (state, action) => {
+    switch(action.type) {
+        case "ADD":
+            return state + action.number;
+        case "SUBTRACT":
+            return state - action.number;
+        case "MULTIPLY":
+            return state * action.number;
+        case "DEVIDE":
+            return state / action.number;
+        default: 
+            return state;
+    }
 
-
+}
 
 
 
 export const UseReducerCalculator = () => {
+    const [counter, dispatch] = useReducer(reducer, 1);
+
     return (
         <div>
             <h1>CALCULATOR USING useREDUCER!</h1>
