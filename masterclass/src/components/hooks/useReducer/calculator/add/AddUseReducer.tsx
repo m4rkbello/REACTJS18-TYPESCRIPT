09729340fa571aafdcc58789a06dedad3e233da0
m4rkbello <markbello10041999@gmail.com>
2024-06-17 18:44:33 +0800

    HAD ERROR

--- masterclass/src/components/hooks/useReducer/calculator/add/AddUseReducer.tsx
+++ masterclass/src/components/hooks/useReducer/calculator/add/AddUseReducer.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Dispatch } from "react";
+import { IAction } from "../UseReducerCalculator"; // Adjust the import path accordingly
 
+interface Props {
+    dispatch: Dispatch<IAction>;
+}
 
-export const AddUserReducer = () => {
+export const AddUseReducer: React.FC<Props> = ({ dispatch }) => {
     return (
         <div>
             <h1>ADD USE REDUCER</h1>
+            <button onClick={() => dispatch({ type: "ADD", number: 1 })}>Add 1</button>
         </div>
     )
 }
