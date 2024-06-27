ca1cc069ad17febac8d75bad4dd98a6da2964afb
m4rkbello <markbello10041999@gmail.com>
2024-06-27 21:39:19 +0800

    useReducer

--- masterclass/src/components/hooks/useReducer/calculator/devide/DevideUseReducer.tsx
+++ masterclass/src/components/hooks/useReducer/calculator/devide/DevideUseReducer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import { Dispatch } from "react";
+import { IAction } from "../UseReducerCalculator";
 
-export const DevideUseReducer = () => {
+
+interface Props {
+    dispatch: Dispatch<IAction>;
+}
+
+export const DevideUseReducer: React.FC<Props> = ({ dispatch }) => {
+
     return (
         <div>
             <h1>DEVIDE USE REDUCER HOOK!</h1>
